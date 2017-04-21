1e3c7b4c308c1b2625b4f04c27ed21f59a3540b5
etienneplatini <etienne.platini@skycha.com>
2017-04-21 16:46:14 +0200

    tests with gzip and bzip2

--- scripts/general_options.sh
+++ scripts/general_options.sh
@@ -27,6 +27,79 @@ DESCRIPTION="check if vsearch is in the PATH"
 
 #*****************************************************************************#
 #                                                                             #
+#                         Options --bzip2_decompress                          #
+#                                                                             #
+#*****************************************************************************#
+
+## bzip2 is installed
+OUTPUT=$(mktemp)
+DESCRIPTION="bzip2 is installed"
+which bzip2 > "${OUTPUT}"
+[[ -s "${OUTPUT}" ]] && \
+    success "${DESCRIPTION}" || \
+        failure "${DESCRIPTION}"
+rm "${OUTPUT}"
+
+## Creating a bzip2 file
+## We write in tmp file that we then zip and erase
+## Then we create a handle for the compressed file created
+BZIP2_FILE=$(mktemp)
+for ((i=1 ; i<=100 ; i++)) ; do
+    printf ">%s%d\nAAGG\n+\nAAGG\n" "seq" ${i}
+done > "${BZIP2_FILE}"
+bzip2 "${BZIP2_FILE}" -c > BZIP2_FILE_NAME
+rm "${BZIP2_FILE}"
+BZIP2_FILE=BZIP2_FILE_NAME
+
+## --bzip2 is accepted
+#DESCRIPTION="--bzip2 is accepted"
+#printf "${BZIP2_FILE}" | \
+#"${VSEARCH}" --fastq_chars - --bzip2_decompress &> /dev/null && \
+#    success "${DESCRIPTION}" || \
+#       failure "${DESCRIPTION}"
+
+## Removing bzip2 file
+rm "${BZIP2_FILE}"
+
+#*****************************************************************************#
+#                                                                             #
+#                         Options --bzip2_decompress                          #
+#                                                                             #
+#*****************************************************************************#
+
+## gzip is installed
+OUTPUT=$(mktemp)
+DESCRIPTION="gzip is installed"
+which gzip > "${OUTPUT}"
+[[ -s "${OUTPUT}" ]] && \
+    success "${DESCRIPTION}" || \
+        failure "${DESCRIPTION}"
+rm "${OUTPUT}"
+
+## Creating a gzip file
+## We write in tmp file that we then zip and erase
+## Then we create a handle for the compressed file created
+GZIP_FILE=$(mktemp)
+for ((i=1 ; i<=100 ; i++)) ; do
+    printf ">%s%d\nAAGG\n+\nAAGG\n" "seq" ${i}
+done > "${GZIP_FILE}"
+gzip "${GZIP_FILE}" -c > GZIP_FILE_NAME
+rm "${GZIP_FILE}"
+GZIP_FILE=GZIP_FILE_NAME
+
+## --gzip is accepted
+DESCRIPTION="--gzip is accepted"
+printf "${GZIP_FILE}" | \
+"${VSEARCH}" --fastq_chars - --gzip_decompress &> /dev/null && \
+    success "${DESCRIPTION}" || \
+       failure "${DESCRIPTION}"
+
+## Removing gzip file
+rm "${GZIP_FILE}"
+
+
+#*****************************************************************************#
+#                                                                             #
 #                        Options --version and --help                         #
 #                                                                             #
 #*****************************************************************************#
@@ -71,15 +144,21 @@ rm "${OUTPUT}"
 #*****************************************************************************#
 
 ## --quiet is accepted
-OUTPUT=$(mktemp)
 DESCRIPTION="--quiet is accepted"
-printf '@a_1\nACGT\n+\n@JJh\n' | \
+printf '@a\nACGT\n+\n@JJh\n' | \
+    "${VSEARCH}" --fastq_chars - --quiet &> /dev/null && \
+    success "${DESCRIPTION}" || \
+        failure "${DESCRIPTION}"
+
+## --quiet actually shrink the output
+OUTPUT=$(mktemp)
+DESCRIPTION="--quiet actually shrink the output"
+printf '@a\nAAAA\n+\naaaa\n' | \
     "${VSEARCH}" --fastq_chars - --quiet 2> "${OUTPUT}"
-[[ -s "${OUTPUT}" ]] && \
-    failure "${DESCRIPTION}" || \
-        success "${DESCRIPTION}"
+COUNT=$(wc -l < "${OUTPUT}")
+[[ "${COUNT}" = "12" ]] && \
+    success "${DESCRIPTION}" || \
+        failure "${DESCRIPTION}"
 rm "${OUTPUT}"
-
-
 
 exit 0
