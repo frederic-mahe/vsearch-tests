9489ee4c7c48cd15b4550e6767b49d7ed21c3b8f
Frédéric Mahé <frederic.mahe@cirad.fr>
2023-02-21 17:48:09 +0100

    activate --derep_fulllength tests

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -3,6 +3,7 @@
 ## Launch all tests
 for s in fastq_parsing.sh \
              test_accepted_chars.sh \
+             derep_fulllength.sh \
              fixed_bugs.sh ; do
     bash "./scripts/${s}" "${1}" || exit 1
     echo
