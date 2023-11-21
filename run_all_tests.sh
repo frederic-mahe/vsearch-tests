78a3cc70592043bcb946da672d0f62c7dcc1a0fe
Frédéric Mahé <frederic.mahe@cirad.fr>
2023-11-21 17:01:16 +0100

    include sortbysize tests

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -4,6 +4,7 @@
 for s in fastq_parsing.sh \
              test_accepted_chars.sh \
              derep_fulllength.sh \
+             sortbysize.sh \
              fixed_bugs.sh ; do
     bash "./scripts/${s}" "${1}" || exit 1
     echo
