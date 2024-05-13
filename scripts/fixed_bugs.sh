742d64ea1de8f978e2e763aa350d8e519a2afd9f
Frédéric Mahé <frederic.mahe@cirad.fr>
2024-05-13 11:26:29 +0200

    progress meter

--- scripts/fixed_bugs.sh
+++ scripts/fixed_bugs.sh
@@ -11610,7 +11610,7 @@ DESCRIPTION="issue 558: usearch_global, missing sample ID (no truncation at '_')
 
 exit 0
 
-# DONE: issues 1 to 30 and 550 to 560
+# DONE: issues 1 to 30 and 549 to 560
 # TODO: issue 529
 # TODO: issue 513: make a test with two occurrences of the query in the target sequence
 # TODO: issue 547: the way kmer profile scores are computed is not clear at all. I cannot predict it.
