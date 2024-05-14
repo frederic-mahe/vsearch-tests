e3f5cffd79914651612fffc18af6d59aaa3263a9
Frédéric Mahé <frederic.mahe@cirad.fr>
2024-05-14 15:46:16 +0200

    issue 40

--- scripts/fixed_bugs.sh
+++ scripts/fixed_bugs.sh
@@ -3016,6 +3016,8 @@ ${VSEARCH} \
 ##
 ## https://github.com/torognes/vsearch/issues/40
 
+# not testable
+
 
 #******************************************************************************#
 #                                                                              #
