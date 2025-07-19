c6ea7114409da4cb5a51c724745fd6068345ad32
Frédéric Mahé <frederic.mahe@cirad.fr>
2025-07-19 09:54:05 +0200

    rename variable, spacing

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
 ## Launch all tests
-for s in vsearch.sh \
-             cut.sh \
-             derep_fulllength.sh \
-             derep_id.sh \
-             derep_prefix.sh \
-             derep_smallmem.sh \
-             fasta2fastq.sh \
-             fastq_chars.sh \
-             fastq_join.sh \
-             fastq_parsing.sh \
-             fastq_stats.sh \
-             fastx_subsample.sh \
-             fastx_uniques.sh \
-             fixed_bugs.sh \
-             help.sh \
-             rereplicate.sh \
-             sff_convert.sh \
-             shuffle.sh \
-             sortbylength.sh \
-             sortbysize.sh \
-             test_accepted_chars.sh \
-             version.sh ; do
-    bash "./scripts/${s}" "${1}" || exit 1
+for test_script in vsearch.sh \
+                       cut.sh \
+                       derep_fulllength.sh \
+                       derep_id.sh \
+                       derep_prefix.sh \
+                       derep_smallmem.sh \
+                       fasta2fastq.sh \
+                       fastq_chars.sh \
+                       fastq_join.sh \
+                       fastq_parsing.sh \
+                       fastq_stats.sh \
+                       fastx_subsample.sh \
+                       fastx_uniques.sh \
+                       fixed_bugs.sh \
+                       help.sh \
+                       rereplicate.sh \
+                       sff_convert.sh \
+                       shuffle.sh \
+                       sortbylength.sh \
+                       sortbysize.sh \
+                       test_accepted_chars.sh \
+                       version.sh ; do
+    bash "./scripts/${test_script}" "${1}" || exit 1
     echo
 done
 
