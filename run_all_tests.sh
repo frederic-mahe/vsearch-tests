3b496de83279d356d83604c69638329d99071345
Frédéric Mahé <frederic.mahe@cirad.fr>
2025-07-19 16:11:20 +0200

    add valgrind test for command --fastx_mask

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -44,6 +44,7 @@ for test_script in allpairs_global.sh \
                        fastx_getseq.sh \
                        fastx_getseqs.sh \
                        fastx_getsubseq.sh \
+                       fastx_mask.sh \
                        maskfasta.sh \
                        search_exact.sh \
                        usearch_global.sh ; do
--- scripts/fastx_mask.sh
+++ scripts/fastx_mask.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash -
 
 ## Print a header
-SCRIPT_NAME="masking options"
+SCRIPT_NAME="fastx_mask"
 LINE=$(printf -- "-%.0s" {1..76})
 printf "# %s %s\n" "${LINE:${#SCRIPT_NAME}}" "${SCRIPT_NAME}"
 
@@ -12,7 +12,7 @@ NO_COLOR="\033[0m"
 
 failure () {
     printf "${RED}FAIL${NO_COLOR}: ${1}\n"
-    # exit 1
+    exit 1
 }
 
 success () {
@@ -28,6 +28,86 @@ DESCRIPTION="check if vsearch is executable"
 [[ -x "${VSEARCH}" ]] && \
     success "${DESCRIPTION}" || \
         failure "${DESCRIPTION}"
+
+
+#*****************************************************************************#
+#                                                                             #
+#                           mandatory options                                 #
+#                                                                             #
+#*****************************************************************************#
+
+## vsearch --fastx_mask fastxfile (--fastaout | --fastqout) outputfile [options]
+
+
+#*****************************************************************************#
+#                                                                             #
+#                            default behaviour                                #
+#                                                                             #
+#*****************************************************************************#
+
+
+#*****************************************************************************#
+#                                                                             #
+#                              core options                                   #
+#                                                                             #
+#*****************************************************************************#
+
+
+#*****************************************************************************#
+#                                                                             #
+#                            secondary options                                #
+#                                                                             #
+#*****************************************************************************#
+
+
+#*****************************************************************************#
+#                                                                             #
+#                              invalid options                                #
+#                                                                             #
+#*****************************************************************************#
+
+# none
+
+#*****************************************************************************#
+#                                                                             #
+#                               memory leaks                                  #
+#                                                                             #
+#*****************************************************************************#
+
+## valgrind: search for errors and memory leaks
+if which valgrind > /dev/null 2>&1 ; then
+
+    LOG=$(mktemp)
+    FASTQ=$(mktemp)
+    printf "@s\nAAAAAA\n+\nIIIIII\n" > "${FASTQ}"
+    valgrind \
+        --log-file="${LOG}" \
+        --leak-check=full \
+        "${VSEARCH}" \
+        --fastx_mask "${FASTQ}" \
+        --fastaout /dev/null \
+        --fastqout /dev/null \
+        --log /dev/null 2> /dev/null
+    DESCRIPTION="--fastx_mask valgrind (no leak memory)"
+    grep -q "in use at exit: 0 bytes" "${LOG}" && \
+        success "${DESCRIPTION}" || \
+            failure "${DESCRIPTION}"
+    DESCRIPTION="--fastx_mask valgrind (no errors)"
+    grep -q "ERROR SUMMARY: 0 errors" "${LOG}" && \
+        success "${DESCRIPTION}" || \
+            failure "${DESCRIPTION}"
+    rm -f "${LOG}" "${FASTQ}"
+fi
+
+
+#*****************************************************************************#
+#                                                                             #
+#                                    notes                                    #
+#                                                                             #
+#*****************************************************************************#
+
+
+exit 0
 
 
 #*****************************************************************************#
