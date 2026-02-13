ea20918983fb19069fc0308f494634f45bac37c3
Frédéric Mahé <frederic.mahe@cirad.fr>
2026-02-13 20:09:36 +0100

    fix test colors (protect octal value)

--- scripts/derep_smallmem.sh
+++ scripts/derep_smallmem.sh
@@ -11,12 +11,12 @@ GREEN="\033[1;32m"
 NO_COLOR="\033[0m"
 
 failure () {
-    printf "%sFAIL%s: %s\n" "${RED}" "${NO_COLOR}" "${1}"
+    printf "%bFAIL%b: %s\n" "${RED}" "${NO_COLOR}" "${1}"
     exit 1
 }
 
 success () {
-    printf "%sPASS%s: %s\n" "${GREEN}" "${NO_COLOR}" "${1}"
+    printf "%bPASS%b: %s\n" "${GREEN}" "${NO_COLOR}" "${1}"
 }
 
 ## use the first binary in $PATH by default, unless user wants
