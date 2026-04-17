ca48bccdf1214e36a729ce21e514caa703e4d799
Frédéric Mahé <frederic.mahe@cirad.fr>
2026-04-17 10:11:29 +0200

    preliminary work

--- scripts/orient.sh
+++ scripts/orient.sh
@@ -37,7 +37,8 @@ DESCRIPTION="check if vsearch is executable"
 #                                                                             #
 #*****************************************************************************#
 
-## vsearch --orient fastxfile --db fastxfile (--fastaout | --fastqout | --notmatched | --tabbedout) outputfile [options]
+# (see the manpage vsearch-orient.1 for more details)
+# vsearch --orient fastxfile --db fastxfile (--fastaout | --fastqout | --notmatched | --tabbedout) outputfile [options]
 
 
 #*****************************************************************************#
@@ -46,6 +47,64 @@ DESCRIPTION="check if vsearch is executable"
 #                                                                             #
 #*****************************************************************************#
 
+# typical usage of vsearch --orient
+
+# for some tests, the input sequences need to be long enough. The
+# orientation algorithm works on 12-mers, and requires that one strand
+# shares 4× more words than the other to avoid an undetermined
+# result. With very short sequences, you'll frequently get ?
+# (undetermined) results rather than + or -.
+
+# This is the biggest practical trap. The # todo: note at the bottom
+# hints at this but doesn't give Claude enough guidance to avoid
+# it. You should explicitly tell Claude that test sequences need to be
+# at least ~30–50 nt long and crafted so that k-mer ratios are
+# unambiguous, or provide a known-good example pair.
+
+
+# 12-nt is enough to trigger a forward match with db sequence (Forward oriented sequences)
+SEQ="GACAGGTACAAG"
+vsearch \
+    --orient <(printf ">s\n%s\n" "${SEQ}") \
+    --db <(printf ">s\n%s\n" "${SEQ}") \
+    --fastaout -
+unset SEQ
+
+# 12-nt is enough to trigger a reverse match with db sequence (Reverse oriented sequences)
+SEQ1="GACAGGTACAAG"
+SEQ2="CTTGTACCTGTC"  # reverse-complement of SEQ1 (all k-mers match on
+                     # the reverse strand, and zero on the forward )
+vsearch \
+    --orient <(printf ">s\n%s\n" "${SEQ1}") \
+    --db <(printf ">s\n%s\n" "${SEQ2}") \
+    --fastaout -
+unset SEQ1 SEQ2
+
+# to use simple sequences, we need to use masking
+SEQ="AAAAAAAAAAAA"
+vsearch \
+    --orient <(printf ">s\n%s\n" "${SEQ}") \
+    --qmask "none" \
+    --dbmask "none" \
+    --db <(printf ">s\n%s\n" "${SEQ}") \
+    --fastaout -
+unset SEQ
+
+# use this python code to generate sequences with a precise number of 12-mers:
+
+# for example: SEQ="GACAGGTACAAGAAGGAGTATGCAT"
+
+# python3 -c "
+# import random, sys
+# random.seed(42)
+# bases = 'ACGT'
+# while True:
+#     seq = ''.join(random.choices(bases, k=25))
+#     kmers = [seq[i:i+12] for i in range(len(seq)-11)]
+#     if len(kmers) == len(set(kmers)):
+#         print(seq)
+#         break
+# "
 
 #*****************************************************************************#
 #                                                                             #
@@ -60,6 +119,14 @@ DESCRIPTION="check if vsearch is executable"
 #                                                                             #
 #*****************************************************************************#
 
+
+#*****************************************************************************#
+#                                                                             #
+#                              ignored options                                #
+#                                                                             #
+#*****************************************************************************#
+
+# option --threads goes here
 
 #*****************************************************************************#
 #                                                                             #
