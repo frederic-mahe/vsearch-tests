2f118583aed7182f00e6e402159cf3d8b92b4730
Frédéric Mahé <frederic.mahe@cirad.fr>
2026-04-24 14:50:14 +0200

    all commands are tested

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -4,7 +4,12 @@
 
 ## command-specific tests
 for test_script in vsearch.sh \
+                       allpairs_global.sh \
                        chimeras_denovo.sh \
+                       cluster_fast.sh \
+                       cluster_size.sh \
+                       cluster_smallmem.sh \
+                       cluster_unoise.sh \
                        cut.sh \
                        derep_fulllength.sh \
                        derep_id.sh \
@@ -13,7 +18,14 @@ for test_script in vsearch.sh \
                        fasta2fastq.sh \
                        fastq_chars.sh \
                        fastq_convert.sh \
+                       fastq_eestats.sh \
+                       fastq_eestats2.sh \
+                       fastq_filter.sh \
                        fastq_join.sh \
+                       fastx_filter.sh \
+                       fastx_getseq.sh \
+                       fastx_getseqs.sh \
+                       fastx_getsubseq.sh \
                        fastq_mergepairs.sh \
                        fastq_stats.sh \
                        fastx_mask.sh \
@@ -21,13 +33,23 @@ for test_script in vsearch.sh \
                        fastx_subsample.sh \
                        fastx_uniques.sh \
                        help.sh \
+                       makeudb_usearch.sh \
                        maskfasta.sh \
                        rereplicate.sh \
+                       search_exact.sh \
                        sff_convert.sh \
                        shuffle.sh \
                        sintax.sh \
                        sortbylength.sh \
                        sortbysize.sh \
+                       uchime_denovo.sh \
+                       uchime2_denovo.sh \
+                       uchime3_denovo.sh \
+                       uchime_ref.sh \
+                       udb2fasta.sh \
+                       udbinfo.sh \
+                       udbstats.sh \
+                       usearch_global.sh \
                        version.sh ; do
     bash "./scripts/${test_script}" "${1}" || exit 1
     echo
@@ -37,34 +59,6 @@ done
 for test_script in fastq_parsing.sh \
                        fixed_bugs.sh \
                        test_accepted_chars.sh ; do
-    bash "./scripts/${test_script}" "${1}" || exit 1
-    echo
-done
-
-## (preliminary) command-specific tests (valgrind)
-# goal: all commands have at least a valgrind test
-for test_script in allpairs_global.sh \
-                       cluster_fast.sh \
-                       cluster_size.sh \
-                       cluster_smallmem.sh \
-                       cluster_unoise.sh \
-                       fastq_eestats.sh \
-                       fastq_eestats2.sh \
-                       fastq_filter.sh \
-                       fastx_filter.sh \
-                       fastx_getseq.sh \
-                       fastx_getseqs.sh \
-                       fastx_getsubseq.sh \
-                       makeudb_usearch.sh \
-                       search_exact.sh \
-                       uchime_denovo.sh \
-                       uchime2_denovo.sh \
-                       uchime3_denovo.sh \
-                       uchime_ref.sh \
-                       udb2fasta.sh \
-                       udbinfo.sh \
-                       udbstats.sh \
-                       usearch_global.sh ; do
     bash "./scripts/${test_script}" "${1}" || exit 1
     echo
 done
